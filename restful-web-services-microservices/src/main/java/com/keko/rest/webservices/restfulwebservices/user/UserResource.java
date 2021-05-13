b1733bfb048ab3d5d09777f3bcaf6e00514456e0
Clériston Martinelo <cleriston@gmail.com>
2021-05-13 10:43:36 -0300

    24. Step 11 - Implementing Exception Handling - 404 Resource Not Found

--- restful-web-services-microservices/src/main/java/com/keko/rest/webservices/restfulwebservices/user/UserResource.java
+++ restful-web-services-microservices/src/main/java/com/keko/rest/webservices/restfulwebservices/user/UserResource.java
@@ -25,7 +25,10 @@ public class UserResource {
 
 	@GetMapping(path = "/users/{id}")
 	public User retriveUser(@PathVariable Integer id ) {		
-		return service.findOne(id);
+		User user = service.findOne(id);
+		if (user == null)
+			throw new UserNotFoundException("id: "+id);
+		return user;
 	}
 	
 	@PostMapping("/users")
